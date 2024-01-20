97b9c467212ea6ea39efd36fdb908b62e1554b66
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2024-01-20 18:41:47 +0100

    Added models, migrations, seeders

---
+++ database/seeders/LotImageSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LotImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
