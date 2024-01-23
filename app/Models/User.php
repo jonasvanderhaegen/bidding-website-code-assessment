b68777e935594287158c651d0c513550ebaf2f63
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2024-01-23 13:24:32 +0100

    Added admin boolean to variable casts

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,7 @@ class User extends Authenticatable
     ];
 
     protected $casts = [
+        'admin' => 'boolean',
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
