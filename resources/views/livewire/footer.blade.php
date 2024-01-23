9fbf3c3913affcd43b91d0985de654e816f7fc49
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2024-01-23 14:20:00 +0100

    Adjusted footer, adjusted logo href link to route home and added html attribute wire:navigate to it

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,10 +1,9 @@
 <footer class="p-4 bg-white md:p-8 lg:p-10">
     <div class="mx-auto max-w-screen-xl text-center">
-        <a href="#" class="flex justify-center items-center text-2xl font-semibold text-gray-900">
-            <span class="text-3xl text-black dark:text-white"><i><strong class="text-blue-400">BID</strong>VIBE</i></span>
-
+        <a wire:navigate href="{{route('home')}}" class="flex justify-center items-center text-2xl font-semibold text-gray-900">
+            <span class="text-2xl text-black dark:text-white"><i><strong class="text-blue-400">BID</strong>VIBE</i></span>
         </a>
-        <p class="my-6 text-gray-500">Regularly organizing unique and unexpected auctions, making each bid an exciting discovery journey.</p>
+        <p class="my-6 text-gray-500">Regularly organizing unique and unexpected auctions, <br> making each bid an exciting discovery journey.</p>
 
         <span class="text-sm text-gray-500 sm:text-center">© 2024 <a href="#" class="hover:underline">BidVibe™</a>. All Rights Reserved.</span>
     </div>
