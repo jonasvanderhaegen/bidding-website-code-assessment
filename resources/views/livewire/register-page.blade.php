46bb1d619fc17086b42e9462d710402266c88f2d
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2024-01-23 14:15:22 +0100

    Adjusted the width of the login and register box from full width to box, and color of buttons and links

--- resources/views/livewire/register-page.blade.php
+++ resources/views/livewire/register-page.blade.php
@@ -1,6 +1,6 @@
 <section class="">
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0">
+        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 rounded-lg shadow">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                     Create an account
@@ -38,7 +38,7 @@
                             <input wire:model="form.terms_accepted" id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required="">
                         </div>
                         <div class="ml-3 text-sm">
-                            <label for="terms" class="font-light text-gray-500">I accept the <a wire:navigate class="font-medium text-primary-600 hover:underline" href="{{route('terms-conditions')}}">Terms and Conditions</a></label>
+                            <label for="terms" class="font-light text-gray-500">I accept the <a wire:navigate class="font-medium text-blue-600 hover:underline" href="{{route('terms-conditions')}}">Terms and Conditions</a></label>
                         </div>
 
                         @error('terms_accepted')
@@ -46,9 +46,9 @@
                         @enderror
 
                     </div>
-                    <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                    <button type="submit" class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
                     <p class="text-sm font-light text-gray-500">
-                        Already have an account? <a wire:navigate href="{{ route('login') }}" class="font-medium text-primary-600 hover:underline">Login here</a>
+                        Already have an account? <a wire:navigate href="{{ route('login') }}" class="font-medium text-blue-600 hover:underline">Login here</a>
                     </p>
                 </form>
             </div>
