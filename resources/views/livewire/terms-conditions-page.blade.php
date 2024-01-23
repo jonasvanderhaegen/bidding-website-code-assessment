bd2dc2387b69404f3b68e0abff8345b8177f3033
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2024-01-23 14:16:17 +0100

    Adjust content of terms and conditions

--- resources/views/livewire/terms-conditions-page.blade.php
+++ resources/views/livewire/terms-conditions-page.blade.php
@@ -1,3 +1,29 @@
+<!--
+Install the "flowbite-typography" NPM package to apply styles and format the article content:
+
+URL: https://flowbite.com/docs/components/typography/
+-->
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+    <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+        <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <header class="mb-4 lg:mb-6 not-format">
+
+                <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">Terms and conditions</h1>
+            </header>
+
+            <p class="lead mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi metus ex, suscipit eget tempus sed, sollicitudin dapibus magna. Etiam vel volutpat enim. Aenean at diam ut velit laoreet sodales. Donec eget elit ante. Curabitur pharetra vehicula finibus. Pellentesque vitae eros laoreet, rhoncus ex eu, scelerisque tortor. Nullam aliquet lobortis accumsan. Cras eu molestie lorem. Vivamus pretium mattis tellus sodales egestas. Sed mattis orci a erat lobortis auctor. Morbi in vulputate erat.</p>
+
+            <p class="mb-5">In sed dictum lectus, dignissim rhoncus orci. Fusce ac quam ac erat laoreet tempor a id enim. Integer pellentesque porta cursus. Sed nec eros ligula. Nullam posuere urna erat, eu facilisis lacus sagittis eget. Aenean accumsan neque id placerat bibendum. Mauris vestibulum tincidunt mollis. Proin nec lorem vitae lectus maximus placerat.</p>
+
+            <p class="mb-5">Pellentesque posuere, eros eu finibus condimentum, tellus est maximus quam, non feugiat sem felis nec magna. Donec aliquet vitae dui vitae sodales. Sed laoreet ante id mi ultricies vestibulum. Fusce pellentesque tortor gravida, scelerisque ante sit amet, dapibus arcu. Mauris vestibulum ultricies nisi a dictum. Quisque est arcu, lobortis vel eros at, laoreet tincidunt purus. Phasellus placerat viverra leo quis gravida. Cras ultricies tellus vel purus rutrum, nec pellentesque ex suscipit. Quisque a ipsum vitae dui ultrices auctor. Proin tempus felis justo, sed porttitor ante varius vel. Nulla eget eros nec leo tempus tincidunt.</p>
+
+            <p class="mb-5">Nunc eu mauris id diam suscipit condimentum. Vestibulum sagittis sapien massa, et rutrum leo tempor sit amet. Phasellus placerat nisi vitae neque lacinia maximus. Mauris scelerisque metus ut risus tristique placerat. Curabitur vehicula et ligula sit amet tincidunt. Proin mattis diam non iaculis iaculis. Praesent sed luctus erat. Nulla hendrerit volutpat sodales.</p>
+
+            <p>Etiam id metus non dui aliquet fringilla at pellentesque justo. Pellentesque justo enim, tristique et scelerisque sed, lobortis at mauris. Aliquam sit amet mauris sagittis, dictum mauris eu, eleifend est. Etiam eget laoreet eros, ac aliquet metus. Vestibulum quis elementum ligula, id blandit nibh. Praesent tincidunt leo sed purus fermentum, in auctor mauris dictum. Sed bibendum nulla purus, at scelerisque enim facilisis eu. Phasellus in mi lacinia, vestibulum turpis a, mattis tortor. Integer gravida, velit quis dictum dictum, dolor risus fermentum sapien, vel fermentum ex nisi ac enim. Duis sed egestas dolor. Phasellus pulvinar eros eu sem sagittis, id interdum mi euismod.</p>
+
+        </article>
+    </div>
+</main>
+
 </div>
