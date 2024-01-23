a14b528042958ef51e754658939bb642a79d2792
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2024-01-23 14:32:00 +0100

    Shorten remember label value

--- resources/views/livewire/login-page.blade.php
+++ resources/views/livewire/login-page.blade.php
@@ -26,7 +26,7 @@
                                 <input wire:model="form.remember" id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
                             </div>
                             <div class="ml-3 text-sm">
-                                <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                <label for="remember" class="text-gray-500 dark:text-gray-300">Remember</label>
                             </div>
                         </div>
                         <a wire:navigate href="{{ route('password.request') }}" class="text-sm font-medium text-blue-600 hover:underline dark:text-primary-500">Forgot password?</a>
