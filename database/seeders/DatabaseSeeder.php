d988bb3060c91b172f5872ea0222433a34e5677f
Jonas Vanderhaegen <jonasvanderhaegen@hotmail.com>
2024-01-24 09:19:03 +0100

    Migrated to dedicated user seed class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,22 +8,9 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
-            'admin' => true
-        ]);
-
-        \App\Models\User::factory(10)->create();
-
-
+        $this->call(UserSeeder::class);
         $this->call(StateSeeder::class);
         $this->call(LotSeeder::class);
         $this->call(LotImageSeeder::class);
